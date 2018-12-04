d8b5b7114189a2b6dc88e672d6e1c2db51138470
Nick Davis <nickdavisa@gmail.com>
2018-12-04 01:02:35 -0700

    solved sideways triangle using two different methods

---
+++ chapter_02/a_sideways_triangle.py
@@ -0,0 +1,27 @@
+"""
+prints a sideways triangle like so:
+#
+##
+###
+####
+###
+##
+#
+"""
+
+# Did this one using a coditional before reading the explanation in the book
+for i in range(8):
+    if i >= 4:
+        for k in range(1, 8 - i):
+            print('#', end='')
+    else:
+        for j in range(0, 1+i):
+            print('#', end='')
+    print(' row: ', i)
+
+
+# Was able to figure this out after reading the explanation in the book
+for i in range(8):
+    for j in range(0,4 - abs(4-i)):
+        print('#', end='')
+    print(' row = ', i)
